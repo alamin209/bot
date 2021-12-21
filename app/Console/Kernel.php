511bd88c4d90c4e5e116e2516c3e22ac5995e077
Mithun Das <mithuncse404@gmail.com>
2021-12-22 00:45:32 +0600

    second commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         
-        $schedule->command('email:send')->everyTwoHours();
+        $schedule->command('email:send')->everyMinute();
     }
 
     /**
